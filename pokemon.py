3a181a2a98dbbe9bae9b7bb6d8a9e798be36856e
Matt-Littlefield <littlefield02@gmail.com>
2022-04-06 15:39:25 -0400

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -5,20 +5,20 @@ from PIL import ImageTk,Image
 #Create root screen
 root = Tk()
 root.title("Pokemon")
-root.geometry("1000x800")
+root.geometry("1920x1200")
+root.config(bg="blue")
 
 #Open background image
+"""
 bg = Image.open('background.png')
-
-background = Canvas(root)
-background.pack(fill = "both")
-bg = ImageTk.PhotoImage(bg)
-background.create_image(100,100,image=bg)
-
-
+bg = bg.resize((1920,1200))
+background = ImageTk.PhotoImage(bg)
+label1 = Label(root, image = background)
+label1.pack(expand=True,fill='both')
+"""
 #Open and place pokemon logo
 img1= Image.open('pokemon.png')
-img1 = img1.resize((2650//4,942//4))
+img1 = img1.resize((2650//10,942//10))
 canvas = Canvas(root,width=10000,height=10000)
 canvas.pack()
 img = ImageTk.PhotoImage(img1)
