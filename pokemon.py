c1cf8c777d93ac3db19991559d629c12010691fe
Matt-Littlefield <littlefield02@gmail.com>
2022-04-07 15:00:48 -0400

    added test file

--- pokemon.py
+++ pokemon.py
@@ -9,20 +9,29 @@ root.geometry("1920x1200")
 root.config(bg="blue")
 
 #Open background image
-"""
-bg = Image.open('background.png')
-bg = bg.resize((1920,1200))
-background = ImageTk.PhotoImage(bg)
-label1 = Label(root, image = background)
-label1.pack(expand=True,fill='both')
-"""
+
+#bg = Image.open('background.png')
+#bg = bg.resize((1920,1200))
+#background = ImageTk.PhotoImage(bg)
+#label1 = Label(root, image = background)
+#label1.pack(expand=True,fill='both')
+bg = PhotoImage(file="background.png")
+label = Label(root,image=bg)
+label.place(x=0,y=0,relwidth=1,relheight=1)
+
+
 #Open and place pokemon logo
+"""
 img1= Image.open('pokemon.png')
 img1 = img1.resize((2650//10,942//10))
 canvas = Canvas(root,width=10000,height=10000)
 canvas.pack()
 img = ImageTk.PhotoImage(img1)
-canvas.create_image(500,400, image=img)
+canvas.create_image(500,500, image=img)
+"""
+
+
+
 
 
 
