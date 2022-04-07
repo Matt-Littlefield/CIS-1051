2c6988b953bf01ab837222fa8c643d25b41a1ca3
Matt-Littlefield <littlefield02@gmail.com>
2022-04-07 15:07:30 -0400

    cleaned pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -1,37 +1,26 @@
-from textwrap import fill
 from tkinter import *
 from PIL import ImageTk,Image
 
 #Create root screen
 root = Tk()
-root.title("Pokemon")
-root.geometry("1920x1200")
-root.config(bg="blue")
+root.title("Test")
+root.geometry("960x600")
 
-#Open background image
+#Create canvas
+canvas = Canvas(root,width=960,height=600,)
+canvas.pack(fill="both",expand=True)
 
-#bg = Image.open('background.png')
-#bg = bg.resize((1920,1200))
-#background = ImageTk.PhotoImage(bg)
-#label1 = Label(root, image = background)
-#label1.pack(expand=True,fill='both')
-bg = PhotoImage(file="background.png")
-label = Label(root,image=bg)
-label.place(x=0,y=0,relwidth=1,relheight=1)
+#Open and create background image
+img = Image.open('background.png')
+img = img.resize((960,600))
+bg = ImageTk.PhotoImage(img)
+canvas.create_image(0,0,image=bg, anchor=NW)
 
-
-#Open and place pokemon logo
-"""
-img1= Image.open('pokemon.png')
-img1 = img1.resize((2650//10,942//10))
-canvas = Canvas(root,width=10000,height=10000)
-canvas.pack()
-img = ImageTk.PhotoImage(img1)
-canvas.create_image(500,500, image=img)
-"""
-
-
-
+#Open and create title image
+title = Image.open('pokemon.png')
+title = title.resize((320,118))
+title = ImageTk.PhotoImage(title)
+title_window = canvas.create_image(320,100,anchor="nw",image=title)
 
 
 
