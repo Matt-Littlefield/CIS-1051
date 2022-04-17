2ebe34d010c28dc1e0da09cfb70296f041e27601
Matt-Littlefield <littlefield02@gmail.com>
2022-04-17 13:15:11 -0400

    creating selection screen

--- pokemon.py
+++ pokemon.py
@@ -1,13 +1,15 @@
 from tkinter import *
+from tkinter import ttk
 from PIL import ImageTk,Image
 
 #Function to change screen
 def homeToSelect():
     canvas.delete("all")
+    selectionScreen()
 
 #Create root screen
 root = Tk()
-root.title("Test")
+root.title("Pokemon")
 root.geometry("960x600")
 
 #Create canvas
@@ -34,8 +36,30 @@ def homeScreen():
     homeButton = Button(root,text="Click to Start",command=homeToSelect)
     canvas.create_window(450,450,anchor=NW,window=homeButton)
 
-def 
-homeScreen()
+#Creates selection screen
+def selectionScreen():
+    #Background
+    #canvas.configure(bg='gray')
+
+    #Dropdowns
+    """
+    frame = Frame(root)
+    frame.pack()
+    values = ['1','2','3']
+    list1 = ttk.Combobox(frame,values = values)
+    list1.set("Pick a Pokemon")
+    list1.pack(padx=100,pady=100)
+    canvas.create_window(450,450,anchor=NW,window=list1)
+    """
+    options = ['1','2','3']
+    clicked = StringVar()
+    clicked.set("Pick a Pokemon")
+    drop = OptionMenu(root,clicked,*options)
+    canvas.create_window(450,450,anchor=NW,window=drop)
+
+    
+
+selectionScreen()
 
 root.mainloop()
 
