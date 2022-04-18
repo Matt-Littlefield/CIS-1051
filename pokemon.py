a80ac1f1227b00af589a88f7c048f59215a7baa0
Matt-Littlefield <littlefield02@gmail.com>
2022-04-18 13:08:29 -0400

    gui updates

--- pokemon.py
+++ pokemon.py
@@ -1,11 +1,14 @@
 from tkinter import *
-from tkinter import ttk
+import pyglet, tkinter
 from PIL import ImageTk,Image
 
 #Function to change screen
 def homeToSelect():
     canvas.delete("all")
     selectionScreen()
+
+def selectToBattle():
+    canvas.delete("all")
 
 #Create root screen
 root = Tk()
@@ -30,7 +33,7 @@ def homeScreen():
     title = Image.open('pokemon.png')
     title = title.resize((320,118))
     title = ImageTk.PhotoImage(title)
-    canvas.create_image(320,100,anchor="nw",image=title)
+    canvas.create_image(320,100,anchor=NW,image=title)
 
     #Button for home screen
     homeButton = Button(root,text="Click to Start",command=homeToSelect)
@@ -38,26 +41,35 @@ def homeScreen():
 
 #Creates selection screen
 def selectionScreen():
+    global diaBox,bg
     #Background
-    #canvas.configure(bg='gray')
+    canvas.configure(bg='gray')
+    
+    #big text box
+    box = Image.open('dialog box.png')
+    box = box.resize((924,422))
+    diaBox = ImageTk.PhotoImage(box)
+    canvas.create_image(20,125,anchor=NW,image=diaBox)
 
     #Dropdowns
-    """
-    frame = Frame(root)
-    frame.pack()
-    values = ['1','2','3']
-    list1 = ttk.Combobox(frame,values = values)
-    list1.set("Pick a Pokemon")
-    list1.pack(padx=100,pady=100)
-    canvas.create_window(450,450,anchor=NW,window=list1)
-    """
     options = ['1','2','3']
     clicked = StringVar()
-    clicked.set("Pick a Pokemon")
-    drop = OptionMenu(root,clicked,*options)
-    canvas.create_window(450,450,anchor=NW,window=drop)
+    clicked.set("Pick Your Pokemon")
+    drop1 = OptionMenu(root,clicked,*options)
+    canvas.create_window(100,240,anchor=NW,window=drop1)
 
+    options = ['4','5','6']
+    clicked1 = StringVar()
+    clicked1.set("Pick Opponent's Pokemon")
+    drop2 = OptionMenu(root,clicked1,*options)
+    canvas.create_window(100,340,anchor=NW,window=drop2)
     
+    #Label
+    canvas.create_text(500,40,text="Pick Your Pokemon",font=("Helvetica",35))
+
+    #Battle Button
+    battleButton = Button(root,text="Battle!",command=selectToBattle)
+    canvas.create_window(480,550,anchor=NW,window=battleButton)
 
 selectionScreen()
 
